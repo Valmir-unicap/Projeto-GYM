bdaebfe3d41aa6e75957c1a0e0468a301648cc74
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-05-03 09:05:05 -0300

    Create script.js

---
+++ Projeto GYM/FrontEnd/script.js
@@ -0,0 +1,15 @@
+        // const exercicios = document.querySelectorAll(".exercicio");
+        // exercicios.forEach( (exercicio) =>{
+        //     exercicio.style.display = "none";
+        // });
+
+        const exibirExercicio = ( (exercicio) => {
+            const exercicios = document.querySelectorAll(".exercicio");
+            exercicios.forEach( (exercicio) =>{
+                exercicio.style.display = "none";
+            });          
+            const exercicioSelecionado = document.querySelector(".exercicio_" + exercicio);
+            exercicioSelecionado.style.display = "flex";
+        });
+
+    exibirExercicio('triceps');
