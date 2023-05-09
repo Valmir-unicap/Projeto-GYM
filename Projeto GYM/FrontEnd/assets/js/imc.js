87ec8c6ad828bf5c382bcbafaf494f28f36fe102
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2023-05-09 10:12:07 -0300

    Testing consumer API

--- Projeto GYM/FrontEnd/assets/js/imc.js
+++ Projeto GYM/FrontEnd/assets/js/imc.js
@@ -2,47 +2,18 @@ function calcularIMC() {
     const altura = document.getElementById("altura").value;
     const peso = document.getElementById("peso").value;
 
-   // const data = {
-    //     "height": "2", 
-    //     "weight": "21"
-    // };
-    
-    // let options = {
-    //     method: "POST",              
-    //     body: data,
-    // };
-
-    // const formData = new FormData();
-    // formData.append("height", altura);
-    // formData.append("weight", peso);
-
-    // const options = {
-    //     method: "POST",
-    //     body: formData
-    // };
-
-
-    const url = "https://calculate-imc-api-git-main-gabrielle-1.vercel.app/calculate";
-
-    // fetch(url, options)
-    // .then(response => response.json())
-    // .then(data => console.log(data))
-    // .catch(error => console.error(error));
+    const url = "https://calculate-imc-api-git-main-gabrielle-1.vercel.app";
 
     $.ajax({
         url: url,
-        method: "POST",
-        dataType: "json",
-        body: {
-            "height": 10,
-            "weight": 10
+        type: "GET",        
+        success: function(data) {
+            console.log(data);
         },
-        success: function(response) {
-            console.log(response);           
-        },
-        error: function(error) {
-            console.error(error);
-            alert("Erro ao buscar CEP");
+        error: function(xhr, status, error) {
+            console.log(xhr);
+            console.log("status" + status);
+            console.log(error);
         }
     });
 }
