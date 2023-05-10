02605941a81a5ec80bd1473dfd1575d775af81a8
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2023-05-10 19:06:09 -0300

    Added a popup to IMC

--- Projeto GYM/FrontEnd/assets/js/imc.js
+++ Projeto GYM/FrontEnd/assets/js/imc.js
@@ -1,19 +1,53 @@
+// Função para fazer uma requisição à API calculate-imc
 function calcularIMC() {
     const altura = document.getElementById("altura").value;
     const peso = document.getElementById("peso").value;
 
-    const url = "https://calculate-imc-api-git-main-gabrielle-1.vercel.app";
+    if (altura == "" || altura == null || peso == "" || peso == null) {
+        alert("Por favor, verifique se você digitou os valores de altura e peso corretamente.");
+    } else {
 
-    $.ajax({
-        url: url,
-        type: "GET",        
-        success: function(data) {
-            console.log(data);
-        },
-        error: function(xhr, status, error) {
-            console.log(xhr);
-            console.log("status" + status);
-            console.log(error);
+        const data = {
+            height: altura,
+            weight: peso
         }
-    });
+    
+        const url = "https://calculate-imc-api-git-main-gabrielle-1.vercel.app/calculate";
+    
+        $.ajax({
+            url: url,
+            type: "POST",        
+            data: data,
+            success: function(data) {
+                if (data.interpretation != null && data.interpretation != "") {
+                    const interpretation = data.interpretation;
+                    
+                    const popup = document.querySelector('.popup-content');
+                    const informationIMC = `
+                        <h1>${interpretation.status}</h1>                                   
+                        <p>${interpretation.message}</p>
+                        <button class="close-button" onclick="closePopup()">Fechar</button>
+                    `;
+                    popup.innerHTML = informationIMC;                
+                    openPopup();               
+                }
+            },
+            error: function(xhr, status, error) {
+                alert("Ocorreu um erro ao calcular o seu IMC. Tente novamente mais tarde ou entre em contato!");
+                console.log(error);
+            }
+        });
+        
+    }
+
+}
+
+function openPopup() {
+    var popupOverlay = document.getElementById("popupOverlay");
+    popupOverlay.style.display = "flex";
+}
+
+function closePopup() {
+    var popupOverlay = document.getElementById("popupOverlay");
+    popupOverlay.style.display = "none";
 }
